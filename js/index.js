716f364295788d17a1cd3a4092109c4cf9ac35a6
Boffeer <41575886+Boffeer@users.noreply.github.com>
2021-08-15 19:12:24 +0300

    make popups

--- js/index.js
+++ js/index.js
@@ -1,37 +1,58 @@
 $(document).ready(function () {
+  $(document).on("click", 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top,
+      },
+      500
+    );
+  });
+
   $(".market-reviews-slider").slick({
     centerMode: true,
     slidesToShow: 1,
     infinite: true,
     dots: true,
     arrows: true,
+    speed: 900,
   });
+
   $(".market-reviews-slider").on("beforeChange", function () {
     var currentSlide = $(this).slick("slickCurrentSlide") + 1;
     var nextSlide = currentSlide + 1;
     var prevSlide = currentSlide - 1;
+    setTimeout(function () {
+      $(".market-reviews-slider-slide--next").removeClass(
+        "market-reviews-slider-slide--next"
+      );
+      $(".market-reviews-slider-slide--prev").removeClass(
+        "market-reviews-slider-slide--prev"
+      );
 
-    // $(".market-reviews-slider-slide--next").removeClass(
-    //   "market-reviews-slider-slide--next"
-    // );
-    // $(".market-reviews-slider-slide--prev").removeClass(
-    //   "market-reviews-slider-slide--prev"
-    // );
-
-    // $(".market-reviews-slider-slide.slick-current")
-    //   .next()
-    //   .addClass("market-reviews-slider-slide--next");
-    // $(".market-reviews-slider-slide.slick-current")
-    //   .prev()
-    //   .addClass("market-reviews-slider-slide--prev");
-
-    // $('.market-reviews-slider-slide[data-slick-index="' + nextSlide + '"]'
-    // ).addClass("market-reviews-slider-slide--next");
-    // $(
-    //   '.market-reviews-slider-slide[data-slick-index="' + prevSlide + '"]'
-    // ).addClass("market-reviews-slider-slide--prev");
-    console.log(prevSlide, currentSlide, nextSlide);
+      $(".market-reviews-slider-slide.slick-current")
+        .next()
+        .addClass("market-reviews-slider-slide--next");
+      $(".market-reviews-slider-slide.slick-current")
+        .next()
+        .next()
+        .addClass("market-reviews-slider-slide--next");
+      $(".market-reviews-slider-slide.slick-current")
+        .prev()
+        .addClass("market-reviews-slider-slide--prev");
+      $(".market-reviews-slider-slide.slick-current")
+        .prev()
+        .prev()
+        .addClass("market-reviews-slider-slide--prev");
+    }, 100);
+    // console.log(prevSlide, currentSlide, nextSlide);
   });
+  $('.market-reviews-slider-slide[data-slick-index="-1"]').addClass(
+    "market-reviews-slider-slide--prev"
+  );
+  $('.market-reviews-slider-slide[data-slick-index="1"]').addClass(
+    "market-reviews-slider-slide--next"
+  );
 
   /*
   	Toggle Dropdown
@@ -51,7 +72,6 @@ $(document).ready(function () {
               .parent()
               .find("." + dropDownBottomClassName)
               .slideUp();
-            console.log(dropDownBottomClassName);
             $(this)
               .parent()
               .removeClass(
@@ -84,4 +104,28 @@ $(document).ready(function () {
     ".market-faq-dropdowns-dropdown-top",
   ];
   handleDropdown(dropdownsClassesArray);
+
+  /**
+   * Popups
+   */
+
+  poppa({
+    pop: ".market-popups-callback",
+    clickTrigger: ".market-header__btn",
+    animation: "slide-down",
+    closerType: "outer",
+  });
+
+  poppa({
+    pop: ".market-popups-bundle--standard",
+    clickTrigger: ".market-pricing-table-bundle__enroll--standard",
+    animation: "zoom-in",
+    closerType: "outer",
+  });
+  poppa({
+    pop: ".market-popups-bundle--base",
+    clickTrigger: ".market-pricing-table-bundle__enroll--base",
+    animation: "zoom-in",
+    closerType: "outer",
+  });
 });
